26ff69cc28e383f2f9bf3c84a6cba556349a9ae7
Shubham Mehta <shubham@Shubhams-MacBook-Air.local>
2026-03-25 23:31:30 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,25 @@ const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
-require("dotenv").config();
 
 const app = express();
 
-// ✅ IMPORTANT FIX
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+// CORS
+app.use(cors());
 
+// Middleware
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
